8f82a9594a6baae2564269715264e6b0b774cc03
Tehreem Fazili <tehreemfazili5@gmail.com>
2023-04-11 10:58:38 +0530

    added contact form to the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About/>
+      <Contact/>
     </div>
   );
 }
